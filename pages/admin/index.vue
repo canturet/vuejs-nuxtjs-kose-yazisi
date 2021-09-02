197dc4aa11b76f4b7e17c383760a41397520b67a
unknown <turetcan@gmail.com>
2021-09-02 16:32:04 +0300

    admin layout ve admin sayfası oluşturuldu

---
+++ pages/admin/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="container d-flex justify-content-center align-items-center">
+      <button class="btn btn-dark">Yeni Bir Yazı Ekle</button>
+    </div>
+    <PostList />
+  </div>
+</template>
+<script>
+import PostList from "@/components/post/PostList";
+export default {
+  layout: "admin",
+  components: {
+    PostList,
+  },
+};
+</script>
+<style scoped></style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <PostList />
-    <About />
   </div>
 </template>
 <script>
