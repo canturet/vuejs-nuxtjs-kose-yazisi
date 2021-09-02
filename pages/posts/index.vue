86e9621d63f9bb33185ccbfe4184ef30ab98e708
unknown <turetcan@gmail.com>
2021-09-02 16:23:13 +0300

    post list sayfası oluşturuldu

---
+++ pages/posts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <PostList />
+    <About />
+  </div>
+</template>
+<script>
+import PostList from "@/components/post/PostList";
+export default {
+  components: {
+    PostList
+  },
+};
+</script>
+<style scoped></style>
