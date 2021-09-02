fedd8a337d9d4df8d61e07763a6e2e0936d62a3f
unknown <turetcan@gmail.com>
2021-09-02 16:17:02 +0300

    post, post list ve about componentleri oluşturuldu

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
-<template></template>
-<script></script>
+<template>
+  <div>
+    <PostList />
+    <About />
+  </div>
+</template>
+<script>
+import PostList from "@/components/post/PostList";
+import About from "@/components/home/About";
+export default {
+  components: {
+    PostList,
+    About,
+  },
+};
+</script>
 <style scoped></style>
