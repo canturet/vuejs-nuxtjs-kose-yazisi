0b24a457f60636f1c4d532b86fecb09e3b4d652d
unknown <turetcan@gmail.com>
2021-09-03 10:59:32 +0300

    post detay sayfası dinamik hale getirildi

--- pages/posts/_postId/index.vue
+++ pages/posts/_postId/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <PostDetail />
+  <PostDetail :post="fetchedPost"/>
 </template>
 <script>
 import PostDetail from "@/components/post/PostDetail";
@@ -7,5 +7,18 @@ export default {
   components: {
     PostDetail,
   },
+  asyncData(context,callback){
+    setTimeout(() => {
+      callback(null,{
+        fetchedPost : {
+          id: 1,
+            title: "Başlık 1 -"+context.params.postId,
+            subTitle: "Alt Başlık 1",
+            text: "Açıklama 1",
+            author: "Yazar 1",
+        }
+      })
+    }, 1000);
+  }
 };
 </script>
