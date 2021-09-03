778e0db8c9097f135b2f1eba9a69f9286fe9dd87
unknown <turetcan@gmail.com>
2021-09-03 15:03:35 +0300

    firebase üzerinden belirli bir yazıyı görüntüleme işlemi tamamlandı

--- pages/posts/_postId/index.vue
+++ pages/posts/_postId/index.vue
@@ -2,23 +2,19 @@
   <PostDetail :post="fetchedPost"/>
 </template>
 <script>
+import axios from "axios" 
 import PostDetail from "@/components/post/PostDetail";
 export default {
   components: {
     PostDetail,
   },
-  asyncData(context,callback){
-    setTimeout(() => {
-      callback(null,{
-        fetchedPost : {
-          id: 1,
-            title: "Başlık 1 -"+context.params.postId,
-            subTitle: "Alt Başlık 1",
-            text: "Açıklama 1",
-            author: "Yazar 1",
+  asyncData(context){
+    return axios.get("https://nuxt-js-kose-yazisi-default-rtdb.firebaseio.com/posts/"+context.params.postId+".json")
+      .then(response => {
+        return {
+          fetchedPost : response.data
         }
       })
-    }, 1000);
   }
 };
 </script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,28 +9,11 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        fetchedPosts: [
-          {
-            id: 1,
-            title: "Başlık 1_1",
-            subTitle: "Alt Başlık 1_1",
-            text: "Açıklama 1_1",
-            author: "Yazar 1_1",
-          },
-          {
-            id: 2,
-            title: "Başlık 2_1",
-            subTitle: "Alt Başlık 2_1",
-            text: "Açıklama 2_1",
-            author: "Yazar 2_1",
-          },
-        ],
-      });
-    }, 1000);
-  },
+  computed : {
+    fetchedPosts(){
+      return this.$store.getters.getPosts
+    }
+  }
 };
 </script>
 <style scoped></style>
