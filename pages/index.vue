473ff29cc0dd0021329559fde20be62738a1e417
unknown <turetcan@gmail.com>
2021-09-03 12:22:56 +0300

    verilerin store üzerinden aktarılması sağlandı

--- pages/index.vue
+++ pages/index.vue
@@ -12,36 +12,10 @@ export default {
     PostList,
     About,
   },
-  created(){
-    this.$store.dispatch("setPosts",this.fetchedPosts)
-  },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      resolve({
-        fetchedPosts: [
-          {
-            id: 1,
-            title: "Başlık 1",
-            subTitle: "Alt Başlık 1",
-            text: "Açıklama 1",
-            author: "Yazar 1",
-          },
-          {
-            id: 2,
-            title: "Başlık 2",
-            subTitle: "Alt Başlık 2",
-            text: "Açıklama 2",
-            author: "Yazar 2",
-          },
-        ],
-      });
-    })
-      .then(data => {
-        return data
-      })
-      .catch(e => {
-        context.error(e);
-      });
+  computed: {
+    fetchedPosts() {
+      return this.$store.getters.getPosts;
+    },
   },
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -2,21 +2,39 @@ import Vuex from "vuex";
 
 const createStore = () => {
     return new Vuex.Store({
-        state : {
-            fetchedPosts : []
+        state: {
+            fetchedPosts: []
         },
-        mutations : {
-            setPosts(state,posts){
+        mutations: {
+            setPosts(state, posts) {
                 state.fetchedPosts = posts
             }
         },
-        actions : {
-            setPosts(vuexContext, posts){
-                vuexContext.commit("setPosts",posts)
+        actions: {
+            nuxtServerInit(vuexContext, context) {
+                vuexContext.commit("setPosts", [
+                    {
+                        id: 1,
+                        title: "Başlık 1",
+                        subTitle: "Alt Başlık 1",
+                        text: "Açıklama 1",
+                        author: "Yazar 1",
+                    },
+                    {
+                        id: 2,
+                        title: "Başlık 2",
+                        subTitle: "Alt Başlık 2",
+                        text: "Açıklama 2",
+                        author: "Yazar 2",
+                    },
+                ])
+            },
+            setPosts(vuexContext, posts) {
+                vuexContext.commit("setPosts", posts)
             }
         },
-        getters : {
-            getPosts(state){
+        getters: {
+            getPosts(state) {
                 return state.fetchedPosts
             }
         }
