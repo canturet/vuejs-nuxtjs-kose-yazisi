ee4d2ddcae9a76bbdf748058e0a5a5bb7f5f711e
unknown <turetcan@gmail.com>
2021-09-03 08:44:31 +0300

    formların dinamik olarak veri göndermesi sağlandı

--- pages/admin/new-post.vue
+++ pages/admin/new-post.vue
@@ -1,5 +1,5 @@
 <template>
-  <PostForm />
+  <PostForm @submit="savePost($event)" />
 </template>
 <script>
 import PostForm from "@/components/admin/PostForm";
@@ -7,5 +7,10 @@ export default {
   components: {
     PostForm,
   },
+  methods : {
+    savePost(post){
+      console.log(post)
+    }
+  }
 };
 </script>
